ae7fd7af4f910fcaa607c27fe0968a5f2763f2ae
Jose Martinez <104384850+josem3129@users.noreply.github.com>
2024-05-11 18:20:55 +0000

    update 2.1

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Router = express.Router();
 
 Router.use('/', require('./swagger'))
-Router.use('/lego', require('./StarWarsRoute'))
-Router.use('/user', require('./HarryPotterRoute'))
+Router.use('/StarWars', require('./StarWarsRoute'))
+Router.use('/HarryPotter', require('./HarryPotterRoute'))
 
 module.exports = Router;
